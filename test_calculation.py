774ceb9ee2cc4834baa796e986097e4cdcf8344a
VladShtefan1 <shtefanvlad@knu.ua>
2024-04-12 15:36:18 +0300

    додав Тест для функції calculate_population_change test_calculate_population_change

--- test_calculation.py
+++ test_calculation.py
@@ -22,3 +22,15 @@ def test_read_population_data(tmp_path, sample_population_data):
     data = read_population_data(file_path)
     # Перевіряємо, чи отримані дані співпадають з очікуваними
     assert data == sample_population_data
+
+# Тест для функції calculate_population_change
+def test_calculate_population_change(sample_population_data):
+    # Очікуваний результат обчислення зміни популяції
+    expected_result = {
+        'Country1': {2001: 10, 2002: 10},
+        'Country2': {2001: 10, 2002: 10}
+    }
+    # Викликаємо функцію calculate_population_change для тестування
+    result = calculate_population_change(sample_population_data)
+    # Перевіряємо, чи отриманий результат співпадає з очікуваним
+    assert result == expected_result
